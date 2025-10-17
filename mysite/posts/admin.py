3f41cd998ab3562d6f51f44f3f67772bf53a570f
anton <antonbybrest@gmail.com>
2025-10-17 14:06:18 +0200

    lab3

--- mysite/posts/admin.py
+++ mysite/posts/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
+from unicodedata import category
 
-from .models import Category, Topic
+from .models import Category, Topic, Post
 
 
-admin.site.register(Category)
-admin.site.register(Topic)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_filter = ('name',)
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'created')
+    list_filter = ('category', 'name')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at',)
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ('title', 'topic_with_category', 'text_preview', 'created_at', 'updated_at', 'created_by')
+    list_filter = ('topic', 'topic__category', 'created_by', 'topic__name', 'topic__category__name')
+
+    @admin.display(description='Topic', ordering='topic__name')
+    def topic_with_category(self, obj):
+        return f"{obj.topic.name} ({obj.topic.category.name})"
+
+    def text_preview(self, obj):
+        words = (obj.text or "").split()
+        head = " ".join(words[:5])
+        return head + ("..." if len(words) > 5 else "")
+    text_preview.short_description = 'Tekst (5 słów)'
+    text_preview.admin_order_field = 'text'
--- mysite/posts/models.py
+++ mysite/posts/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,8 +6,41 @@ class Category(models.Model):
     name = models.CharField(max_length=60)
     description = models.TextField(default='')
 
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=60)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Topics'
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        words = (self.text or "").split()
+        head = " ".join(words[:5])
+        return head + ("..." if len(words) > 5 else "")
